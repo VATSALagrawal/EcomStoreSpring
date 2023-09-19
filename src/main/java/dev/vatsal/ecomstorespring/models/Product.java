321058fb0cf48cb60848d0c70b369720a14a4a16
Vatsal <vatsal3121agrawal@gmail.com>
2023-09-19 20:18:53 +0530

    Added UUID and Spring data JPA

--- src/main/java/dev/vatsal/ecomstorespring/models/Product.java
+++ src/main/java/dev/vatsal/ecomstorespring/models/Product.java
@@ -1,14 +1,20 @@
 package dev.vatsal.ecomstorespring.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel{
     private String title;
     private String description;
     private String image;
+
+    @ManyToOne
     private Category category;
+
     private double price;
 }
